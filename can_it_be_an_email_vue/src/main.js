52083f79a14ef9154c40cb19e6231de1a2ac508c
catarina-lagrifa <catarinalagrifa@gmail.com>
2021-11-14 10:00:45 +0000

    created v-click-outside directive

--- can_it_be_an_email_vue/src/main.js
+++ can_it_be_an_email_vue/src/main.js
@@ -1,7 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './assets/assets'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./assets/assets";
 
-createApp(App).use(store).use(router).mount('#app')
+app.directive("click-outside", {
+  beforeMount(el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event, el);
+      }
+    };
+    document.body.addEventListener("click", el.clickOutsideEvent);
+  },
+  unmounted(el) {
+    document.body.removeEventListener("click", el.clickOutsideEvent);
+  },
+});
+
+createApp(App).use(store).use(router).mount("#app");
