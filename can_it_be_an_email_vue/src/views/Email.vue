b5a7344426f6f9a13f39d766c73ec1b54a55026b
Rafaela Teixeira Osawe <rafaela.osawe@gmail.com>
2021-11-14 12:53:33 +0000

    label chnages

--- can_it_be_an_email_vue/src/views/Email.vue
+++ can_it_be_an_email_vue/src/views/Email.vue
@@ -5,8 +5,8 @@
     <p>An email allows everyone to <strong>find their own time to read, process and respond</strong> to the issue.</p>
     <p>If you are worried about the urgency of the situation just <strong>mark your email as important</strong> and, for bonus points, shoot a message to your co-workers letting them know.</p>
     <h3>Want to know how much the meeting would cost your company?</h3>
-    <label>Average hourly rate of the attendees</label>
-    <h4>The total cost would be £25</h4>
+    <h4>Average hourly rate of the attendees</h4>
+    <h5>The total cost would be £25</h5>
   </div>
 </template>
 
